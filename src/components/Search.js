e08d0d84b15f6cd0265a5d0dbfd8d28f0be93784
Beata <beata.czekaj.7@gmail.com>
2020-09-20 14:11:24 +0200

    added useEffect hook

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const Search = () => {
+  const [term, setTerm] = useState("");
+
+  useEffect(() => {
+    console.log("effect");
+  }, [term]);
+
   return (
     <div>
       <div className="ui form">
         <div className="field">
           <label>Enter search term</label>
-          <input className="input" />
+          <input
+            value={term}
+            onChange={(e) => setTerm(e.target.value)}
+            className="input"
+          />
         </div>
       </div>
     </div>
