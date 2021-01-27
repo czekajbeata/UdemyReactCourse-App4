c7788233c15f1b9b90c234bb68b6fb9bc3067f40
Beata <beata.czekaj.7@gmail.com>
2021-01-27 17:23:03 +0100

    finalized Search component

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,7 +3,18 @@ import axios from "axios";
 
 const Search = () => {
   const [term, setTerm] = useState("programming");
+  const [debouncedTerm, setDebouncedTerm] = useState(term);
   const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setDebouncedTerm(term);
+    }, 1000);
+
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [term]);
 
   useEffect(() => {
     const searchWiki = async () => {
@@ -13,26 +24,13 @@ const Search = () => {
           list: "search",
           origin: "*",
           format: "json",
-          srsearch: term,
+          srsearch: debouncedTerm,
         },
       });
       setResults(data.query.search);
     };
-
-    if (term && !results.length) {
-      searchWiki();
-    } else {
-      const timeoutId = setTimeout(() => {
-        if (term) {
-          searchWiki();
-        }
-      }, 500);
-
-      return () => {
-        clearTimeout(timeoutId);
-      };
-    }
-  }, [term]);
+    searchWiki();
+  }, [debouncedTerm]);
 
   const renderedResults = results.map((result) => {
     return (
